edfd8ea2733df1664bf94d4316fd7f2440bbb58b
dsujoydev <dsujoy.cse@gmail.com>
2025-06-23 18:02:14 +0600

    chore: remove .gitignore file to clean up project structure

---
+++ server/main.py
@@ -0,0 +1,17 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello, World!"}
+
+def main():
+    print("Hello from server!")
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
+if __name__ == "__main__":
+    main()
