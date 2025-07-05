c9d42c55fc4ad73029d5b369fee03b78a7f6d7cb
dsujoydev <dsujoy.cse@gmail.com>
2025-07-05 23:47:39 +0600

    feat(router): integrate TanStack Router for improved routing and code splitting in the frontend application

---
+++ frontend/src/routes/index.tsx
@@ -0,0 +1,10 @@
+import Home from "@/modules/home/Home";
+import { createFileRoute } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/")({
+  component: Home,
+});
+
+// function RouteComponent() {
+//   return <div>Hello "/"!</div>
+// }
